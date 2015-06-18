614ce7e6fcb8d881b0067a86e7f0742a35333a3c
rina <rina.kamijo@avasys.jp>
2015-06-18 00:52:34 +0000

    :fish:

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,19 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:               'smtp.gmail.com',
+    port:      	           587,
+    domain:                'example.com',
+    user_name:             'minomus.kanjikun@gmail.com',
+    password:              '1qaz2wsx2015',
+    authentication:        'plain',
+    enable_starttls_auto:  true
+  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -35,14 +47,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
-  ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-　:address => "smtp.gmail.com",
-　:port => 587,
-:domain => "gmail.com", # ドメイン設定してなければコメントアウトしてＯＫ
-　:authentication => :login,
-　:user_name => "minomus.kanjikun@gmail.com",
-　:password => "1qaz2wsx2015"
-}
+
   
 end
